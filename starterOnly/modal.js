bcca40802b27dea9626354a1f006223db2e019e3
Matt-Fr <80825384+Matt-Fr@users.noreply.github.com>
2021-10-05 15:43:30 +0200

    added functions to check errors

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,53 +29,138 @@ closeModal.addEventListener("click", function () {
   modalbg.style.display = "none";
 });
 
-// error message firstName
+// check if first name is valid
 
-form.addEventListener("click", () => {
+function checkFirstName() {
   const inputValueFirst = document.getElementById("first").value.trim();
 
   if (inputValueFirst.length < 2) {
     document.getElementsByClassName("error-msg")[0].style.display = "block";
+    return false;
   } else {
     document.getElementsByClassName("error-msg")[0].style.display = "none";
+    return true;
   }
-});
+}
 
-// error message lastname
+// check if last name is valid
 
-form.addEventListener("click", () => {
+function checkLastName() {
   const inputValueLast = document.getElementById("last").value.trim();
 
   if (inputValueLast.length < 2) {
     document.getElementsByClassName("error-msg")[1].style.display = "block";
+    return false;
   } else {
     document.getElementsByClassName("error-msg")[1].style.display = "none";
+    return true;
   }
-});
+}
 
-//email adress regex
+// check if the first checkbox is checked
 
-form.addEventListener("click", () => {
-  const inputValueEmail = document.getElementById("email").value;
-});
-
-//check Age with regex
-
-//checkbox
-
-form.addEventListener("click", () => {
+function checkCheckbox() {
   const checkbox = document.getElementById("checkbox1");
-  // const checkboxChecked = checkbox.checked
-
+  const checkboxChecked = checkbox.checked;
   if (checkbox.checked === false) {
-    document.getElementsByClassName("error-msg")[2].style.display = "block";
+    document.getElementsByClassName("error-msg")[5].style.display = "block";
+    return false;
   } else {
-    document.getElementsByClassName("error-msg")[2].style.display = "none";
+    document.getElementsByClassName("error-msg")[5].style.display = "none";
+    return true;
   }
-});
+}
 
-//prevent form to close
+//prevent form to close if errors
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  if (
+    checkFirstName() == false &&
+    checkLastName() == false &&
+    checkboxChecked() == false
+  ) {
+    e.preventDefault();
+  } else {
+    alert("thank you");
+  }
 });
+
+// error message firstName
+
+// form.addEventListener("click", () => {
+//   const inputValueFirst = document.getElementById("first").value.trim();
+
+//   if (inputValueFirst.length < 2) {
+//     document.getElementsByClassName("error-msg")[0].style.display = "block";
+//     return false;
+//   } else {
+//     document.getElementsByClassName("error-msg")[0].style.display = "none";
+//     return true;
+//   }
+// });
+
+// // error message lastname
+
+// form.addEventListener("click", () => {
+//   const inputValueLast = document.getElementById("last").value.trim();
+
+//   if (inputValueLast.length < 2) {
+//     document.getElementsByClassName("error-msg")[1].style.display = "block";
+//   } else {
+//     document.getElementsByClassName("error-msg")[1].style.display = "none";
+//   }
+// });
+
+// //email adress regex
+// const inputValueEmail = document.getElementById("email").value;
+
+// function isEmail(inputValueEmail) {
+//   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+//     inputValueEmail
+//   );
+// }
+
+// form.addEventListener("click", () => {
+//   if (!isEmail(inputValueEmail)) {
+//     document.getElementsByClassName("error-msg")[2].style.display = "block";
+//   } else {
+//     document.getElementsByClassName("error-msg")[2].style.display = "none";
+//   }
+// });
+
+// // check number of tournaments
+
+// form.addEventListener("click", () => {
+//   const inputValueQuantity = document.getElementById("quantity").value.trim();
+
+//   if (inputValueQuantity == "") {
+//     document.getElementsByClassName("error-msg")[3].style.display = "block";
+//   } else {
+//     document.getElementsByClassName("error-msg")[3].style.display = "none";
+//   }
+// });
+
+// //check Age
+
+// const birthdate = document.getElementById("birthdate").value;
+
+// form.addEventListener("click", () => {
+//   if (birthdate.length !== 10) {
+//     document.getElementsByClassName("error-msg")[4].style.display = "block";
+//   } else {
+//     document.getElementsByClassName("error-msg")[4].style.display = "none";
+//   }
+// });
+
+// //checkbox
+
+// form.addEventListener("click", () => {
+//   const checkbox = document.getElementById("checkbox1");
+//   // const checkboxChecked = checkbox.checked
+
+//   if (checkbox.checked === false) {
+//     document.getElementsByClassName("error-msg")[5].style.display = "block";
+//   } else {
+//     document.getElementsByClassName("error-msg")[5].style.display = "none";
+//   }
+// });
